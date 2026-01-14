c8645157b1382affb6f50644e6385d8b21e63bf0
Karl Horky <karl.horky@gmail.com>
2026-01-14 15:16:25 +0100

    Add first version of Netlify HSTS reproduction

---
+++ proxy.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from 'next/server.js';
+
+export function proxy(request: NextRequest) {
+  const response = NextResponse.next({
+    request: {
+      headers: new Headers(request.headers),
+    },
+  });
+
+  // 30 seconds
+  response.headers.set(
+    'Strict-Transport-Security',
+    'max-age=30; includeSubDomains; preload',
+  );
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for prefetches and requests
+     * starting with:
+     *
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     *
+     * Mostly taken from Next.js docs:
+     * - https://nextjs.org/docs/app/building-your-application/configuring/content-security-policy#:~:text=We%20recommend%20ignoring,the%20CSP%20header.
+     *
+     * Changes from Next.js docs code:
+     * - Removed API Route Handlers, to allow setting security
+     *   headers
+     */
+    {
+      source: '/((?!_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
+};
